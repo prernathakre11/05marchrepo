9f9ccbf9f8cc1f351caacb266e18d28ee4a18d42
prernathakre11 <prernathakre11@gmail.com>
2025-05-13 07:17:22 +0530

    updating a app file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css'; // Importing the CSS file for styles
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="text">Welcome to demo app...</h1>
+    </div>
+  );
+};
+
+export default App;
